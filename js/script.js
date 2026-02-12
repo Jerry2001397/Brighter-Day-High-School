31938715cc1b9a763078fe0567aef384f5a62c1c
Emmanuel Jerry Jr <emmanueljerrylove@gmail.com>
2026-02-12 15:44:07 +0000

    update

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ if (hamburger && navMenu) {
 }
 
 // Navbar Scroll Effect
-window.addEventListener('scroll', () => {
+window.add EventListener ('scroll', () => {
     const navbar = document.querySelector('.navbar');
     if (window.scrollY > 50) {
         navbar.style.boxShadow = '0 5px 20px rgba(0, 0, 0, 0.1)';
